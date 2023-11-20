44dd7ae0df9650f14b858d80e2b66df3b17ee0cf
Mark Olson <115657904+mark-olson@users.noreply.github.com>
2023-11-20 07:04:44 -0800

    DAOSGCP-219 Change provider versions (#94)
    Change provider versions to match versions generated by HPC Toolkit.
    
    Signed-off-by: Mark Olson <115657904+mark-olson@users.noreply.github.com>

--- terraform/modules/daos_server/versions.tf
+++ terraform/modules/daos_server/versions.tf
@@ -15,15 +15,16 @@
  */
 
 terraform {
-  required_version = ">= 0.14.5"
+  required_version = ">= 1.2"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.5.0"
+      version = "~> 4.84.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.5.0"
+      version = "~> 4.84.0"
     }
   }
 }
